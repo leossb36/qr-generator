913eaa6ddbb1f6f70fa78512e960759d8e93a8d1
Leonardo Barreiros <leossb36@gmail.com>
2024-04-23 17:56:05 -0300

    fix: fix limits of qrCode

--- src/modules/generate/dto/property.dto.ts
+++ src/modules/generate/dto/property.dto.ts
@@ -1,14 +1,13 @@
 import { MESSAGES } from '@app/modules/common/constants/message';
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsInt, Min, Max, IsOptional } from 'class-validator';
+import { IsInt, Min, IsOptional } from 'class-validator';
 
 export class QrCodeProperty {
   @ApiProperty({ default: 80, required: false })
   @Type(() => Number)
   @IsInt()
-  @Min(80, { message: MESSAGES.MIN_LIMIT })
-  @Max(500, { message: MESSAGES.MAX_LIMIT })
+  @Min(0, { message: MESSAGES.MIN_LIMIT })
   @IsOptional()
   readonly size?: number = 80;
 }
