df7763ccacfc7df52de713afefb66256b2d9163f
Leonardo Barreiros <leossb36@gmail.com>
2024-04-25 10:48:03 -0300

    fix: removing min qrcode size

--- src/modules/generate/dto/property.dto.ts
+++ src/modules/generate/dto/property.dto.ts
@@ -1,13 +1,11 @@
-import { MESSAGES } from '@app/modules/common/constants/message';
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsInt, Min, IsOptional } from 'class-validator';
+import { IsInt, IsOptional } from 'class-validator';
 
 export class QrCodeProperty {
   @ApiProperty({ default: 80, required: false })
   @Type(() => Number)
   @IsInt()
-  @Min(80, { message: MESSAGES.MIN_LIMIT })
   @IsOptional()
   readonly size?: number = 80;
 }
